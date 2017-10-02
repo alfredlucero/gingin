3767e25a7ac7f92b28afe0b953591808342a1247
alfredlucero <alfred.lucero@ucla.edu>
2017-10-01 17:23:24 -0700

    Refined tablet and desktop view styles

--- gingin-portfolio/js/index.js
+++ gingin-portfolio/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		sectionsColor: ['#B8D5D9', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#D8D8D8'],
 		navigation: true,
-		navigationTooltips: ['Home', 'About Me', 'Currently', 'Past Projects', 'Orgs', 'Reach Out', 'Contact'],
+		navigationTooltips: ['Home', 'About Me', 'Currently', 'Past Projects', 'Edu/Orgs', 'Reach Out', 'Contact'],
 		responsiveWidth: 1024
 	});
 });
