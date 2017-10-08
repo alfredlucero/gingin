d6a7b9688671bfac700b32a410a684354ec4b44e
alfredlucero <alfred.lucero@ucla.edu>
2017-10-08 13:55:36 -0700

    Added home title animations

--- gingin-portfolio/js/index.js
+++ gingin-portfolio/js/index.js
@@ -5,4 +5,20 @@ $(document).ready(function() {
 		navigationTooltips: ['Home', 'About Me', 'Currently', 'Past Projects', 'Edu/Orgs', 'Reach Out', 'Contact'],
 		responsiveWidth: 1024
 	});
+
+	// Wrap every letter in a span
+$('.home-title').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.home-title .letter',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 1500,
+    delay: function(el, i) {
+      return 30 * i;
+    }
+  });
 });
